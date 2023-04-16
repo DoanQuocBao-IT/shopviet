b05cd9814f37904da5f07b5e7dad9097affab03f
doanquocbao <doanquocbao4117@gmail.com>
2023-04-16 20:41:30 +0700

    delete function Seller

--- src/main/java/com/project/shopviet/Controller/SellerController.java
+++ src/main/java/com/project/shopviet/Controller/SellerController.java
@@ -1,14 +1,9 @@
 package com.project.shopviet.Controller;
 
-import com.project.shopviet.Model.Brand;
-import com.project.shopviet.Model.Category;
-import com.project.shopviet.Model.Product;
-import com.project.shopviet.Service.BrandService;
-import com.project.shopviet.Service.CategoryService;
-import com.project.shopviet.Service.ProductService;
-import com.project.shopviet.Service.SellerService;
+import com.project.shopviet.DTO.OrderItemDto;
+import com.project.shopviet.Model.*;
+import com.project.shopviet.Service.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
@@ -19,11 +14,21 @@ import java.util.List;
 public class SellerController {
     @Autowired
     ProductService productService;
+    @Autowired
+    OrderItemService orderItemService;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    ReviewService reviewService;
+    @Autowired
+    FollowService followService;
+    @Autowired
+    UserService userService;
 
 
     @PostMapping("/addProd")
     public Product addProduct(@RequestBody Product product){
-        return productService.addProduct(product);
+        return productService.addProductBySeller(product);
     }
     @DeleteMapping("/delProd/{id}")
     public String deleteProduct(@PathVariable int id){
@@ -32,5 +37,40 @@ public class SellerController {
     @PutMapping("/putProd/{id}")
     public Product updateProduct(@RequestBody Product product,@PathVariable int id){
         return productService.updateProduct(id, product);
+    }
+    @GetMapping("/allOrderItem1")
+    public List<OrderItemDto> getAllOrderItemOfSeller(){
+        return orderItemService.getAllOrderItemOfSeller();
+    }
+    @GetMapping("/allOrder/{seller_id}")
+    List<OrderDetail> getAllOrderBySeller(@PathVariable int seller_id){
+        return orderService.getAllOrderBySeller(seller_id);
+    }
+    @GetMapping("/approved/order{order_id}")
+    OrderItem approvedOrder(@PathVariable int order_id){
+        return orderItemService.updateStatusApproved(order_id);
+    }
+    @PostMapping("/feedback/{review_id}")
+    Review addFeedback(@RequestBody String feedback,@PathVariable int review_id){
+        return reviewService.addReviewFeedback(review_id,feedback);
+    }
+
+
+    @GetMapping("/follow/user{id}")
+    Follow addFollow(@PathVariable int id){
+        return followService.addFollow(id);
+    }
+    @GetMapping("/delfollow/user{id}")
+    void deleteFollow(@PathVariable int id){
+        followService.deteleFollow(id);
+    }
+    @GetMapping("/countfollow/user{id}")
+    int countFollow(@PathVariable int id){
+        return followService.countFollow(id);
+    }
+
+    @GetMapping("/profile")
+    User getProfileShipper(){
+        return userService.getProfile();
     }
 }

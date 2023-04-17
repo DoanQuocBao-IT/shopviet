b7cea3d923a0a743b8206124f863e3f332f53370
doanquocbao <doanquocbao4117@gmail.com>
2023-04-17 09:07:33 +0700

    update security

--- src/main/java/com/project/shopviet/Config/SecurityConfig.java
+++ src/main/java/com/project/shopviet/Config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.project.shopviet.Config;
 
-import com.project.shopviet.JWT.JwtAuthenticationEntryPoint;
 import com.project.shopviet.JWT.JwtAuthenticationFilter;
 import com.project.shopviet.JWT.JwtTokenProvider;
 import com.project.shopviet.JWT.UserDetailsServiceImpl;
@@ -9,20 +8,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     @Autowired
     JwtAuthenticationFilter jwtAuthenticationFilter;
     @Autowired

82cee38f19bbeb38376850bd400c1de3241bfec5
doanquocbao <doanquocbao4117@gmail.com>
2023-12-28 00:07:01 +0700

    update mail

--- src/main/java/com/project/shopviet/Config/MailConfig.java
+++ src/main/java/com/project/shopviet/Config/MailConfig.java
@@ -3,20 +3,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Properties;
 
 @Configuration
 public class MailConfig {
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        mailSender.setHost(host);
+        mailSender.setPort(port);
 
-        mailSender.setUsername("doanquocbao2002@gmail.com ");
-        mailSender.setPassword("hrhcydqyfgogqpjn");
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
